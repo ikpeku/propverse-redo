575ac76d84f7d68c60f2fe4e9d1414a93971f15f
mercyikpe <ikpemercy1@gmail.com>
2024-07-31 16:36:44 +0200

    updated funds purpose inquiry

--- controller/institutional/user.js
+++ controller/institutional/user.js
@@ -1,42 +1,46 @@
 const InstitutionalUser = require('../../model/institutional/user');
-const User = require('../models/user');
 
-exports.updateUserToInstitutionalUser = async (req, res) => {
+exports.fundPurposeInquiry = async (req, res) => {
   try {
-    const { userId } = req.params;
+    console.log("req.payload", req.payload)
+
+    const { userId } = req.payload;
+
     const {
-      funds,
       fund_purpose_inquiry,
       investor_accreditation_status,
       location_of_investment_region,
       capital_already_secured,
     } = req.body;
 
-    // Find the user
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+    let institutionalUser = await InstitutionalUser.findOne({ user: userId });
+
+    if (institutionalUser) {
+      return res.status(400).json({
+        success: false,
+        message: 'Funds purpose inquiry already exists',
+      });
     }
 
-    // Create a new InstitutionalUser document
-    const institutionalUser = new InstitutionalUser({
+    institutionalUser = new InstitutionalUser({
       user: userId,
-      funds,
       fund_purpose_inquiry,
       investor_accreditation_status,
       location_of_investment_region,
       capital_already_secured,
     });
-
     await institutionalUser.save();
 
-    await user.save();
-
-    res
-      .status(201)
-      .json({ message: 'User updated to institutional investor successfully' });
+    res.status(201).json({
+      success: true,
+      data: institutionalUser,
+      message: 'Funds purpose inquiry added successfully',
+    });
   } catch (error) {
-    console.error('Error updating user to institutional investor:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error('Error in addInstitutionalUserInfo:', error);
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
   }
 };
--- routes/institutional/fundRoute.js
+++ routes/institutional/fundRoute.js
@@ -8,9 +8,12 @@ const { getCurrentUser } = require('../../utils/bearerToken');
 const {
   checkInstitutionalUser,
 } = require('../../middleware/users/institutional');
+const { fundPurposeInquiry } = require('../../controller/institutional/user');
 const route = express.Router();
 
 route.post('/fund', getCurrentUser, checkInstitutionalUser, createFund);
+route.post('/fund-inquiry', getCurrentUser, checkInstitutionalUser, fundPurposeInquiry);
+
 route.get('/funds', getAllFunds);
 route.get('/fund/:id', getSingleFund);
 
