109a75e13ae4e32cc499df2916887bda3b72d37a
ikpeku <ikpedaniel07@gmail.com>
2024-09-05 02:13:34 +0100

    add preflight

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
+  app.options('*', cors(corsOptions))
 app.use(cors(corsOptions))
+
 
 // app.use(cors(corsConfigs))
 
