859aa36ca07dace0a3e42d50bf3fe0f748a8ced7
ikpeku <ikpedaniel07@gmail.com>
2024-10-31 06:47:40 +0100

    my investmeent property by id

--- controller/Non_Institional/invest.js
+++ controller/Non_Institional/invest.js
@@ -8,152 +8,152 @@ const { ObjectId } = require("mongodb");
 const { errorHandler } = require("../../utils/error");
 
 
-exports.makeInvestmentOnproperty = async (req, res, next) => {
-  const { userId } = req.params;
-  const {
-    prodId,
-    paid: { amount, currency },
-    proof_of_payment: { location, originalname, mimetype, size, key },
-    payment_status,
-    description,
-    investmentType,
-  } = req.body;
+// exports.makeInvestmentOnproperty = async (req, res, next) => {
+//   const { userId } = req.params;
+//   const {
+//     prodId,
+//     paid: { amount, currency },
+//     proof_of_payment: { location, originalname, mimetype, size, key },
+//     payment_status,
+//     description,
+//     investmentType,
+//   } = req.body;
 
-  try {
-    const response = await Property.findById(prodId);
+//   try {
+//     const response = await Property.findById(prodId);
 
-    if (!response) {
-      return next(errorHandler(400, "confirm transact failed"));
-    }
+//     if (!response) {
+//       return next(errorHandler(400, "confirm transact failed"));
+//     }
 
-    const investment = await Transactions.create({
-      investor: userId,
-      company: response.user,
-      transaction_type: "property purchase",
-      property: prodId,
-      name: response.property_detail.property_overview.property_name,
-      status: "Pending",
-      paid: {
-        amount,
-        currency,
-      },
-      property_amount: {
-        amount: response.property_detail.property_overview.price.amount,
-        currency: response.property_detail.property_overview.price.currency,
-      },
-      proof_of_payment: {
-        location,
-        originalname,
-        mimetype,
-        size,
-        key,
-      },
-      payment_method: "bank  transfer",
-      payment_status,
-      description
-    });
+//     const investment = await Transactions.create({
+//       investor: userId,
+//       company: response.user,
+//       transaction_type: "property purchase",
+//       property: prodId,
+//       name: response.property_detail.property_overview.property_name,
+//       status: "Pending",
+//       paid: {
+//         amount,
+//         currency,
+//       },
+//       property_amount: {
+//         amount: response.property_detail.property_overview.price.amount,
+//         currency: response.property_detail.property_overview.price.currency,
+//       },
+//       proof_of_payment: {
+//         location,
+//         originalname,
+//         mimetype,
+//         size,
+//         key,
+//       },
+//       payment_method: "bank  transfer",
+//       payment_status,
+//       description
+//     });
 
 
-    // if (investmentType === "property") {
+//     // if (investmentType === "property") {
 
-    //   await Non_Institutional_Investor.findByIdAndUpdate(
-    //     userId,
-    //     { $push: { transactions: investment._id, properties: prodId } },
-    //     { new: true, useFindAndModify: false }
-    //   );
+//     //   await Non_Institutional_Investor.findByIdAndUpdate(
+//     //     userId,
+//     //     { $push: { transactions: investment._id, properties: prodId } },
+//     //     { new: true, useFindAndModify: false }
+//     //   );
     
-    //   await Property.findByIdAndUpdate(
-    //   prodId,
-    //     { $push: { transactions: investment._id } },
-    //     { new: true, useFindAndModify: false }
-    //   );
+//     //   await Property.findByIdAndUpdate(
+//     //   prodId,
+//     //     { $push: { transactions: investment._id } },
+//     //     { new: true, useFindAndModify: false }
+//     //   );
 
-    // }
+//     // }
 
-    return res
-      .status(200)
-      .json({
-        status: "success",
-        message: "congratulations record taken awaiting confirmation",
-        // response,
-      });
-  } catch (error) {
-    next(error);
-    // next(errorHandler(400,"confirm transaction failed"))
-  }
-};
+//     return res
+//       .status(200)
+//       .json({
+//         status: "success",
+//         message: "congratulations record taken awaiting confirmation",
+//         // response,
+//       });
+//   } catch (error) {
+//     next(error);
+//     // next(errorHandler(400,"confirm transaction failed"))
+//   }
+// };
 
-exports.makeInvestmentFunds = async (req, res, next) => {
-  const { userId } = req.params;
-  const {
-    prodId,
-    paid: { amount, currency },
-    proof_of_payment: { location, originalname, mimetype, size, key },
-    investmentType,
-    description
-  } = req.body;
+// exports.makeInvestmentFunds = async (req, res, next) => {
+//   const { userId } = req.params;
+//   const {
+//     prodId,
+//     paid: { amount, currency },
+//     proof_of_payment: { location, originalname, mimetype, size, key },
+//     investmentType,
+//     description
+//   } = req.body;
 
 
-  try {
-    const response = await Funds.findById(prodId);
+//   try {
+//     const response = await Funds.findById(prodId);
 
-    if (!response) {
-      return next(errorHandler(400, "confirm transact failed"));
-    }
+//     if (!response) {
+//       return next(errorHandler(400, "confirm transact failed"));
+//     }
 
-    const investment = await Transactions.create({
-      investor: userId,
-      transaction_type: "funds",
-      funds: prodId,
-      name: response.name,
-      status: "Pending",
-      paid: {
-        amount,
-        currency,
-      },
-      property_amount: {
-        amount: response.raise_goal.amount,
-        currency: response.raise_goal.currency,
-      },
-      proof_of_payment: {
-        location,
-        originalname,
-        mimetype,
-        size,
-        key,
-      },
+//     const investment = await Transactions.create({
+//       investor: userId,
+//       transaction_type: "funds",
+//       funds: prodId,
+//       name: response.name,
+//       status: "Pending",
+//       paid: {
+//         amount,
+//         currency,
+//       },
+//       property_amount: {
+//         amount: response.raise_goal.amount,
+//         currency: response.raise_goal.currency,
+//       },
+//       proof_of_payment: {
+//         location,
+//         originalname,
+//         mimetype,
+//         size,
+//         key,
+//       },
 
-      payment_method: "bank  transfer",
-      description
-    });
+//       payment_method: "bank  transfer",
+//       description
+//     });
 
-    // if (investmentType === "funds") {
-    //   await Non_Institutional_Investor.findByIdAndUpdate(
-    //     userId,
-    //     { $push: { transactions: investment._id, funds: prodId } },
-    //     { new: true, useFindAndModify: false }
-    //   );
+//     // if (investmentType === "funds") {
+//     //   await Non_Institutional_Investor.findByIdAndUpdate(
+//     //     userId,
+//     //     { $push: { transactions: investment._id, funds: prodId } },
+//     //     { new: true, useFindAndModify: false }
+//     //   );
       
-    //   await Funds.findByIdAndUpdate(
-    //     prodId,
-    //     { $push: { investments: investment._id } },
-    //     { new: true, useFindAndModify: false }
-    //   );
+//     //   await Funds.findByIdAndUpdate(
+//     //     prodId,
+//     //     { $push: { investments: investment._id } },
+//     //     { new: true, useFindAndModify: false }
+//     //   );
 
-    // }
+//     // }
 
-    return res
-      .status(200)
-      .json({
-        status: "success",
-        message: "congratulations record taken awaiting confirmation",
-        // response,
-      });
-  } catch (error) {
-    next(error);
-    // next(errorHandler(400,"confirm transaction failed"))
-  }
-};
+//     return res
+//       .status(200)
+//       .json({
+//         status: "success",
+//         message: "congratulations record taken awaiting confirmation",
+//         // response,
+//       });
+//   } catch (error) {
+//     next(error);
+//     // next(errorHandler(400,"confirm transaction failed"))
+//   }
+// };
 
 exports.getUserInvestment = async (req, res, next) => {
   const { userId } = req.params;
@@ -174,7 +174,7 @@ exports.getUserInvestment = async (req, res, next) => {
 
   
   try {
-    const allFunds = Non_Institiutional_Investor.aggregate([
+    const investor = Non_Institiutional_Investor.aggregate([
       {
 
         $match: {_id: new ObjectId(userId)}
@@ -230,7 +230,7 @@ exports.getUserInvestment = async (req, res, next) => {
     ]);
 
     const paginationResult = await Non_Institiutional_Investor.aggregatePaginate(
-      allFunds,
+      investor,
       options
     );
 
@@ -250,13 +250,91 @@ exports.getUserInvestment = async (req, res, next) => {
 
 exports.getInvestmentById = async (req, res, next) => {
   const { prodId } = req.params;
+
+  const page = parseInt(req?.query?.page) || 1;
+
+  const limit = parseInt(req?.query?.limit) || 10;
+ 
+  const myCustomLabels = {
+    docs: 'data',
+  };
+
+  const options = {
+    page,
+    limit,
+    customLabels: myCustomLabels
+  };
+
+
+  
   try {
-    // populate("property")
-    const data = await Transactions.findById(prodId);
-    return res.status(200).json({ status: "success", data });
+    const investoredProperty = Property.aggregate([
+      {
+
+        $match: {_id: prodId}
+      },
+      {
+        $lookup: {
+          from: "due_deligences",
+          localField: "company",
+          foreignField: "user",
+          as: "company",
+        }},
+        {
+          "$unwind": "$company"
+        },
+
+        {
+          $lookup: {
+               from: "transactions",
+               localField: "transactions",
+               foreignField: "_id",
+               pipeline: [
+                { $project : {paid:1 , description: 1, paymentDate: {$ifNull: ["$paymentDate", "$createdAt"]}} }
+               ],
+               as: "property_invested",
+             },
+           },
+  
+      {
+        $sort: {
+          updatedAt: -1
+        }
+      },
+      {
+        $project: {
+          // product: "$$ROOT",
+          thumbnail: "$property_detail.property_images",
+          company: "$company.company_information.name",
+          property_name: "$property_detail.property_overview.property_name",
+          property_location: "$property_detail.property_location",
+          property_type: "$property_detail.property_overview.property_type",
+          property_progress: {$ifNull:["$property_progress", 0]} ,
+          property_amount: "$property_detail.property_overview.price",
+          property_dates: "$property_detail.property_overview.date",
+          room_configuration: "$property_detail.property_overview.room_configuration",
+          property_size: "$property_detail.property_overview.size",
+          property_document: "$property_detail.property_documents",
+          payment_update : "$property_invested",
+          activitiees_update : "$activities"
+
+        },
+      },
+    ]);
+
+    const paginationResult = await Property.aggregatePaginate(
+      investoredProperty,
+      options
+    );
+
+    res.status(200).json({
+      success: true,
+       ...paginationResult,
+    });
   } catch (error) {
-    next(errorHandler(400, "failed"));
+    next(errorHandler(500, "server error"));
   }
+
 };
 
 
--- routes/noninstitutional/noninstitutionalRoute.js
+++ routes/noninstitutional/noninstitutionalRoute.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const { userRequestDocs } = require("../../controller/Non_Institional/requestDocs");
 const { contactSupport } = require("../../controller/Non_Institional/contactSupport");
-const { makeInvestmentOnproperty,makeInvestmentFunds, getUserInvestment ,getInvestmentById} = require("../../controller/Non_Institional/invest");
+const { 
+    // makeInvestmentOnproperty,makeInvestmentFunds,
+     getUserInvestment ,getInvestmentById} = require("../../controller/Non_Institional/invest");
 const route = express.Router();
 
 route.post("/request-doc", userRequestDocs)
 route.post("/contact-support/:prodId", contactSupport)
-route.post("/property/invest/:userId", (req, res,next) =>{
-    req.body.investmentType = "property"
-     next()
- }, makeInvestmentOnproperty)
-route.post("/fund/invest/:userId", (req, res,next) =>{
-   req.body.investmentType = "funds"
-    next()
-},
- makeInvestmentFunds)
+// route.post("/property/invest/:userId", (req, res,next) =>{
+//     req.body.investmentType = "property"
+//      next()
+//  }, makeInvestmentOnproperty)
+// route.post("/fund/invest/:userId", (req, res,next) =>{
+//    req.body.investmentType = "funds"
+//     next()
+// },
+//  makeInvestmentFunds)
 
 route.get("/invests/:userId", getUserInvestment)
 route.get("/invest/:prodId", getInvestmentById)
